92d1214726c12b321de6e838935f3368bd552371
Sisinty Sasmita Patra <sisinty.s.patra@intel.com>
2015-10-05 23:20:02 -0700

    Deleted Azure sample

--- AzureSample/create.py
+++
@@ -1,9 +0,0 @@
-from azure.storage import TableService, Entity
-
-table_service = TableService(account_name='iotsample', account_key='0sPMrPyENjiCD8vZ2Y1+5cftT30GcTOafnL/Cdbt67WA0ajba5iEkBCiE9Ajb09UKAAa/cLz0WaYpORutxexeg==')
-
-table_service.create_table('tasktable')
-
-task10 = {'PartitionKey': 'tasksSeattle', 'RowKey': '4', 'description' : 'Buy clothes', 'priority' : 100}
-table_service.insert_entity('tasktable', task10)
-
--- AzureSample/sample.py
+++
@@ -1,7 +0,0 @@
-from azure.storage import TableService, Entity
-
-table_service = TableService(account_name='iotsample', account_key='0sPMrPyENjiCD8vZ2Y1+5cftT30GcTOafnL/Cdbt67WA0ajba5iEkBCiE9Ajb09UKAAa/cLz0WaYpORutxexeg==')
-
-table_service.create_table('tasktable')
-
-
