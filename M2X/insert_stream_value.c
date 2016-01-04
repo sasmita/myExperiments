383f670c530a469fed0e164f7e7d4e3deaa6d496
Sisinty Sasmita Patra <sisinty.s.patra@intel.com>
2016-01-04 03:41:03 -0800

    Modified file to generate json value and insert to stream

--- M2X/insert_stream_value.c
+++ M2X/insert_stream_value.c
@@ -1,21 +1,65 @@
-
 /* 
    Author : Sisinty Sasmita Patra
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "serializer.h"
 
 #include "m2x.h"
 #include "device.h"
 
 const char *M2X_KEY = "CHANGE_THIS_TO_API_KEY"; 
 
+int iso8601_time(char* buf, int len)
+{
+  time_t t;
+
+  t = time(NULL);
+  return strftime(buf, len, "%Y-%m-%dT%H:%M:%SZ", gmtime(&t));
+}
+
+const char* generate_json()
+{
+  json_context *json = NULL;
+  char *json_str, buf[128];
+  int v; 
+
+  json = json_start_object();
+
+  iso8601_time(buf, 128);
+  json = json_pack_key(json, "values");
+  
+  json = json_pack_array_start(json);
+
+  json = json_pack_object_start(json);
+
+  json = json_pack_key(json, "timestamp");
+  json = json_pack_string(json, buf);
+
+  json = json_pack_key(json, "value");
+
+  v = 200;
+  sprintf(buf, "%d", v);
+  json = json_pack_value(json, buf);
+
+  json = json_pack_object_end(json);
+
+  json = json_pack_array_end(json);
+  json_str = json_end_object(json);
+
+  printf("JSON String: %s", json_str);
+
+  return json_str;
+}
+
 int main()
 {
   m2x_context *ctx = NULL;
   m2x_response response;
 
-  /* Create Context */
   ctx = m2x_open(M2X_KEY);
 
   /* Show full verbose/debug information */
@@ -23,10 +67,9 @@ int main()
 
   printf("Stream Post Values:\n");
 
-  /* post the values to the stream */
   response = m2x_device_stream_post_values(ctx, "CHANGE_THIS_TO_DEVICE_ID", 
                                            "temp", 
-					"{\"values\": [{\"timestamp\": \"2015-10-01T12:00:00Z\", \"value\": 80}] }");
+					generate_json());
 
   printf("Status code: %d\n", response.status);
 
@@ -42,5 +85,6 @@ int main()
 
   m2x_release_response(&response);
   m2x_close(ctx);
+
   return 0;
 }
